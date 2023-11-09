0b2ae2ed0ae733b288f9f12b7b95cda519f6db5a
Ravi-FF <ravi1740suthar@gmail.com>
2023-11-09 14:28:12 +0530

    css

---
+++ Project/Amazon/script.js
@@ -0,0 +1,34 @@
+//-------------------- slider------------------
+
+
+let images = document.querySelectorAll(".first-section-img");
+let imagenumber = 0;
+function changeImage(z) {
+    imagenumber = imagenumber + z;
+    firstimageShow(imagenumber);
+}
+firstimageShow(imagenumber);
+function firstimageShow(a) {
+    if (a == images.length) {
+        a = 0;
+        imagenumber = 0;
+    }
+    if (a < 0) {
+        a = images.length - 1;
+        imagenumber = images.length - 1;
+    }
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = "none"
+    }
+    images[a].style.display = "block";
+}
+
+//----------top-left-slider---------------------//
+
+let left_slider = document.querySelector(".left-slider");
+function sliderClicke() {
+left_slider.style.transform="translateX(0)";
+}
+function crossClick() {
+    left_slider.style.transform="translateX(-100%)"
+}
